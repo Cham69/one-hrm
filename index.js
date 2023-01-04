ca285c9ef176e6aa3493b2521a7e32ee4c6c3dc4
Chamaka Jayashan <jayashanchamaka@gmail.com>
2023-01-04 20:11:16 +0530

    Removed sequelize

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
-
+const cors = require("cors");
 const userRoute = require("./src/Routes/userRoutes.js");
+const conn = require("./src/models/db.js");
+
 const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8080"
+  };
+  
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+  
 const PORT = 5000;
+
+app.get('/', (req, res)=>{
+    res.send("Welcome")
+})
 
 //Access the router of users
 app.use('/api/users', userRoute);
